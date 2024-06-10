d809635c67a3c803d0a7cd59edd2145cce857416
tenderrooni <roonifaye@gmail.com>
2024-06-10 12:36:18 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,5 +4,5 @@ console.log("your index.js file is loaded correctly!");
     $('.work').click(function() {
         $('html, body').animate({
             scrollTop: $('.portfolio-section').offset().top
-        }, 1000); // The duration for the animation in milliseconds
+        }, 1000); 
     });
